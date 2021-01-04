ec844fe6a34de8ba35bd986a0c9b2984048678c1
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-04 17:57:50 -0300

    Variáveis e Tipos.

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  public todos: any[] = [];
+
+  constructor() {
+    this.todos.push('Passear com o cachorro');
+    this.todos.push('Ir ao supermercado');
+    this.todos.push('Cortar o cabelo');
+  }
 }
