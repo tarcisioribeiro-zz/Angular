b17d844d8f91265854978d930a6f6ee6a641fdaa
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-04 18:54:53 -0300

    Exibindo Dados.

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
 
   constructor() {
     this.todos.push('Passear com o cachorro');
     this.todos.push('Ir ao supermercado');
     this.todos.push('Cortar o cabelo');
+  }
+
+  alteraTexto(){
+    this.title  = 'Teste';
   }
 }
