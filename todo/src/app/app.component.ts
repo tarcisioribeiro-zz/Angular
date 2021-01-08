c56642b1a2b18fb1d75e2c8838949c9ecc9adbb4
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-08 01:36:25 -0300

    Adicionando uma Tarefa.

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -20,9 +20,17 @@ export class AppComponent {
         Validators.required
       ])]
     });
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
