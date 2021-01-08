91ecf150fb4428b8770c52c4e4a15d4d0d863a9f
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-08 01:14:39 -0300

    Forms Básico.

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,33 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
     this.todos.push(new Todo(1, 'Passear com o cachorro', false));
     this.todos.push(new Todo(2, 'Ir ao supermercado', false));
     this.todos.push(new Todo(3, 'Cortar o cabelo', true));
   }
 
-  remove(todo: Todo){
+  remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
   }
 
-  markAsDone(todo: Todo){
+  markAsDone(todo: Todo) {
     todo.done = true;
   }
 
-  markAsUndone(todo: Todo){
+  markAsUndone(todo: Todo) {
     todo.done = false;
   }
 }
